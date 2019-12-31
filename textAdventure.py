af95f7e12dd36ce71a952964f9249f0541a64079
taraskoffman <59371311+TarasKoffman@users.noreply.github.com>
2019-12-31 11:05:45 +0000

    Mechanics in play

--- textAdventure.py
+++ textAdventure.py
@@ -1,32 +1,169 @@
-answer = input("Would you like to play? (Yes/No)")
-if answer.lower().strip() == "yes":
+# Text Adventure :)
+# By Taras Koffman / Kane Bebb
 
-      print("\nYou walk into a shabby inn on the corner just before the fence "
-            "that surrounded the small town. \n A mangled tree stood over the inn, "
-            "protruding through the roof.")
+import cmd
+import textwrap
+import sys
+import os
+import time
+import random
 
-      print("\n\nAs you enter, you see a hunched old woman with grey hair trailing her feet."
-            "\n She is hobbling over table to table, pouring a steaming liquid into bowls "
-            "\n held by men in armour. You guess that your entrance seems quite the"
-            "\n unusual event to the tavern folk, as all but one of the heads in the inn has"
-            "\n turned towards you, bearing scornful eyes on each. You notice a close-by hand"
-            "\n drifting to its corresponding sword. Before the hand can reach its destination, "
-            "\n the unturned head speaks over the room.")
+screen_width = 100
 
-      print("\n\"Welcome, traveler. \n I believe I didn't catch your name...\"")
-      name = input()
-      print("\n\"Ah," + name + ", it's nice to see a lone traveller "
-                               "on the road every once in a while, "
-                               "reminds me of the old times.\"")
+### Player Setup ###
+class player:
+    def __init__(self):
+        self.name = ''
+        self.hp = 0
+        self.mp = 0
+        self.status_effects = []
+        self.location = 'start'
+myPlayer = player()
 
-      print("\nThe lady turns her head and gives you a wink, or a blink depending on your disposition"
-            "\ntowards such things, as it appears only one eye is present in its socket. She continues "
-            "\nserving the tavern, and by now all the soldiers or mercenaries or whoever they were"
-            "\nhad put away their sword hands and scornful glances, to your relief.")
+### Title Screen ###
+def title_screen_selections():
+    option = input("> ")
+    if option.lower() == "play":
+        start_game() #### This is a placeholder currently
+    elif option.lower() == "help":
+        help_menu() #### Also a placeholder
+    elif option.lower() == "quit":
+        sys.exit()
+    while option.lower() not in ['play', 'help', 'quit']:
+        print('Please enter a valid command')
+        option = input("> ")
+        if option.lower() == "play":
+            start_game()  #
+        elif option.lower() == "help":
+            help_menu()  #
+        elif option.lower() == "quit":
+            sys.exit()
 
-      print("\n\n Do you sit in a dark chair you see to your right or go and order a drink? (Sit/Drink)")
+def title_screen():
+    os.system('clear')
+    print("############################")
+    print("#     Welcome Traveler     #")
+    print("############################")
+    print("          - Play -          ")
+    print("          - Help -          ")
+    print("          - Quit -          ")
+    print("      Made by Taz/Kane      ")
+    title_screen_selections()
 
-else:
-      print("That's too bad")
+def help_menu():
+    print("############################")
+    print("#     Welcome Traveler     #")
+    print("############################")
+    print("- Use up, down, left, and right to move ")
+    print("- Type commands to execute them ")
+    print("- Use 'Look' to inspect something")
+    print("            GLHF :)         ")
+    title_screen_selections()
 
 
+
+###### Game Functionality ######
+def start_game():
+
+##### SAMPLE MAP #####
+
+"""
+ a   b  c  d   ## Player starts at b2 ##
+-------------
+|  |  |  |  |  1
+-------------
+|  |  |  |  |  2
+-------------
+|  |  |  |  |  3
+-------------
+|  |  |  |  |  4
+-------------
+
+"""
+ZONENAME = ""
+DESCRIPTION = "describe"
+EXAMINATION = "examine"
+SOLVED = False
+UP = 'up', 'north'
+DOWN = 'down', 'south'
+LEFT = 'left', 'west'
+RIGHT = 'right', 'east'
+
+solved_places = {'a1': False, 'a2': False, 'a3': False, 'a4': False
+                 'b1': False, 'b2': False, 'b3': False, 'b4': False
+                 'c1': False, 'c2': False, 'c3': False, 'c4': False
+                 'd1': False, 'd2': False, 'd3': False, 'd4': False}
+
+zonemap = {
+    'a1':  {
+    ZONENAME: ""
+    DESCRIPTION = "describe"
+    EXAMINATION = "examine"
+    SOLVED = False
+    UP = 'up', 'north'
+    DOWN = 'down', 'south'
+    LEFT = 'left', 'west'
+    RIGHT = 'right', 'east'
+},
+'a1':  {
+    ZONENAME: ""
+    DESCRIPTION = "describe"
+    EXAMINATION = "examine"
+    SOLVED = False
+    UP = 'up', 'north'
+    DOWN = 'down', 'south'
+    LEFT = 'left', 'west'
+    RIGHT = 'right', 'east'
+},
+
+'a1':  {
+    ZONENAME: ""
+    DESCRIPTION = "describe"
+    EXAMINATION = "examine"
+    SOLVED = False
+    UP = 'up', 'north'
+    DOWN = 'down', 'south'
+    LEFT = 'left', 'west'
+    RIGHT = 'right', 'east'
+},
+
+'a1':  {
+    ZONENAME: ""
+    DESCRIPTION = "describe"
+    EXAMINATION = "examine"
+    SOLVED = False
+    UP = 'up', 'north'
+    DOWN = 'down', 'south'
+    LEFT = 'left', 'west'
+    RIGHT = 'right', 'east'
+},
+
+'a1':  {
+    ZONENAME: ""
+    DESCRIPTION = "describe"
+    EXAMINATION = "examine"
+    SOLVED = False
+    UP = 'up', 'north'
+    DOWN = 'down', 'south'
+    LEFT = 'left', 'west'
+    RIGHT = 'right', 'east'
+},
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
